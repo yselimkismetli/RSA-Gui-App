d6eaeaa6f0f9470b069cd37c10950446e33f047b
Yavuz Selim KISMETLI <mail@yavuzselimkismetli.com>
2021-05-31 21:17:24 +0300

    Project pushed.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace RSAGuiApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlain = new System.Windows.Forms.TextBox();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txtPubKey = new System.Windows.Forms.TextBox();
+            this.txtPrivKey = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPlain
+            // 
+            this.txtPlain.Location = new System.Drawing.Point(12, 67);
+            this.txtPlain.Multiline = true;
+            this.txtPlain.Name = "txtPlain";
+            this.txtPlain.Size = new System.Drawing.Size(420, 71);
+            this.txtPlain.TabIndex = 0;
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(12, 297);
+            this.txtEncrypted.Multiline = true;
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(417, 71);
+            this.txtEncrypted.TabIndex = 1;
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(12, 416);
+            this.txtDecrypted.Multiline = true;
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(608, 71);
+            this.txtDecrypted.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(501, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(501, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 71);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(501, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(119, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label.Location = new System.Drawing.Point(10, 400);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(85, 15);
+            this.label.TabIndex = 6;
+            this.label.Text = "Decrypted Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(10, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Encrypted Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(10, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Plain Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(498, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Key Length";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(632, 54);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "RSA ENCRYPTION APPLICATION";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 502);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(608, 25);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // txtPubKey
+            // 
+            this.txtPubKey.Location = new System.Drawing.Point(13, 167);
+            this.txtPubKey.Multiline = true;
+            this.txtPubKey.Name = "txtPubKey";
+            this.txtPubKey.Size = new System.Drawing.Size(293, 101);
+            this.txtPubKey.TabIndex = 15;
+            // 
+            // txtPrivKey
+            // 
+            this.txtPrivKey.Location = new System.Drawing.Point(327, 167);
+            this.txtPrivKey.Multiline = true;
+            this.txtPrivKey.Name = "txtPrivKey";
+            this.txtPrivKey.Size = new System.Drawing.Size(293, 101);
+            this.txtPrivKey.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(12, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Public Key";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(327, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Private Key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 542);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPrivKey);
+            this.Controls.Add(this.txtPubKey);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.txtPlain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSA Encryption Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlain;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtPubKey;
+        private System.Windows.Forms.TextBox txtPrivKey;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace RSAGuiApp
+{
+    public partial class Form1 : Form
+    {
+
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA;
+
+        byte[] plainText;
+        byte[] encryptedtext;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding, int KeyLen)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(KeyLen))
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding, int KeyLen)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(KeyLen))
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(1024);
+            comboBox1.Items.Add(2048);
+            comboBox1.Items.Add(4096);
+
+            comboBox1.SelectedIndex = 0;
+
+            RSA = new RSACryptoServiceProvider(RSAGetKLen(comboBox1.SelectedIndex));
+
+            txtEncrypted.ScrollBars = ScrollBars.Vertical;
+            txtDecrypted.ScrollBars = ScrollBars.Vertical;
+            txtPrivKey.ScrollBars = ScrollBars.Vertical;
+            txtPubKey.ScrollBars = ScrollBars.Vertical;
+        }
+
+
+        public static int RSAGetKLen(int cb = -1)
+        {
+            int RSAKLen = 0;
+
+            if (cb == 0)
+            {
+                RSAKLen = 1024;
+            }
+            else if (cb == 1)
+            {
+                RSAKLen = 2048;
+            }
+            else if (cb == 2)
+            {
+                RSAKLen = 4096;
+            }
+
+            return RSAKLen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int RSAKLen = 0;
+            RSAKLen = RSAGetKLen(comboBox1.SelectedIndex);
+
+            plainText = ByteConverter.GetBytes(txtPlain.Text);
+
+            encryptedtext = Encryption(plainText, RSA.ExportParameters(false), false, RSAKLen);
+            txtEncrypted.Text = Convert.ToBase64String(encryptedtext);
+
+            progressBar1.Value = 0;
+            progressBar1.Step = 10;
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.PerformStep();
+                Application.DoEvents();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int RSAKLen = 0;
+            RSAKLen = RSAGetKLen(comboBox1.SelectedIndex);
+
+            byte[] decryptedtext = Decryption(encryptedtext, RSA.ExportParameters(true), false, RSAKLen);
+            txtDecrypted.Text = ByteConverter.GetString(decryptedtext);
+
+            progressBar1.Value = 0;
+            progressBar1.Step = 25;
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.PerformStep();
+                Application.DoEvents();
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cmb = (ComboBox)sender;
+            int selectedIndex = cmb.SelectedIndex;
+            RSA = new RSACryptoServiceProvider(RSAGetKLen(Convert.ToInt32(selectedIndex)));
+
+            var privKey = RSA.ToXmlString(true);
+            var pubKey = RSA.ToXmlString(false);
+
+            txtPubKey.Text = pubKey;
+            txtPrivKey.Text = privKey;
+        }
+
+     
+    }
+}
